5c8aeb23b345bd597206f62b7c0376840fcce3d0
Song <Song@DESKTOP-D933NBB.mshome.net>
2019-08-08 15:56:31 +0900

    추가 commit

---
+++ LoveInDaily_Admin/src/main/java/com/kh/lida/admin/model/exception/AdminException.java
@@ -0,0 +1,11 @@
+package com.kh.lida.admin.model.exception;
+
+public class AdminException extends RuntimeException {
+	
+	public AdminException() {}
+	
+	public AdminException(String msg) {
+		super(msg);
+	}
+
+}
